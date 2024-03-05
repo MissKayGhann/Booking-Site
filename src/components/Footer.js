930ddb95009eadb46618db2b2dac53775393ee49
Kareen Ghann <kareenghann@live.com>
2024-03-05 03:18:09 +0000

    restored pages

---
+++ src/components/Footer.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTiktok, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+const Footer = () => {
+  const footerStyle = {
+    backgroundColor: "#644f48",
+    color: "white",
+    padding: "20px",
+    textAlign: "center",
+    marginBottom: "0",
+  };
+
+  const linkStyle = {
+    color: "white",
+    textDecoration: "none",
+    margin: "0 10px",
+  };
+
+  const iconStyle = {
+    margin: "0 5px",
+  };
+
+  return (
+    <footer style={footerStyle}>
+      <p className="kay-glam">Ms Kay Glam © {new Date().getFullYear()}</p>
+      <div>
+        <a href="/terms" style={linkStyle}>
+          Booking Policies
+        </a>{" "}
+        |
+        <a
+          target="_blank"
+          href="https://tiktok.com/@misskayglam"
+          style={linkStyle}
+        >
+          <FontAwesomeIcon icon={faTiktok} style={iconStyle} />
+        </a>
+        <a
+          target="_blank"
+          href="https://instagram.com/mskayglam"
+          style={linkStyle}
+        >
+          <FontAwesomeIcon icon={faInstagram} style={iconStyle} />
+        </a>
+        <a
+          target="_blank"
+          href="mailto:misskayglam@outlook.com"
+          style={linkStyle}
+        >
+          <FontAwesomeIcon icon={faEnvelope} style={iconStyle} />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
