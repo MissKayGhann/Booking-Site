756ba1f38fb85d0b2a63f3ed6232f12e46f1aa3c
Kareen Ghann <kareenghann@live.com>
2024-03-07 10:44:52 +0000

    hash router instead of browser router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import PortfolioPage from "./pages/Portfolio";
 import BookingPage from "./pages/Booking";
