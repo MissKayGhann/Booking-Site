b2d0177fbcaa63f15083d6c0176bb16612b66689
Kareen Ghann <kareenghann@live.com>
2024-03-11 09:53:18 +0000

    font changes and collapsible items on services and terms page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import background from "../images/background.png";
 import fancy from "../images/fancy.png";
 
@@ -9,6 +9,41 @@ const serviceStyle = {
 };
 
 const Services = () => {
+  const [isBrideVisible, setBrideVisible] = useState(false);
+  const [isBridesmaidsVisible, setBridesmaidsVisible] = useState(false);
+  const [isPackagesVisible, setPackagesVisible] = useState(false);
+  const [isInternationalPackagesVisible, setInternationalPackagesVisible] =
+    useState(false);
+
+  const toggleBrideVisibility = () => setBrideVisible(!isBrideVisible);
+  const toggleBridesmaidsVisibility = () =>
+    setBridesmaidsVisible(!isBridesmaidsVisible);
+  const togglePackagesVisibility = () => setPackagesVisible(!isPackagesVisible);
+  const toggleInternationalPackagesVisibility = () =>
+    setInternationalPackagesVisible(!isInternationalPackagesVisible);
+
+  const renderSectionTitle = (title, isVisible) => (
+    <button
+      style={{
+        background: "none",
+        border: "none",
+        display: "flex",
+        alignItems: "center",
+        fontSize: "4rem",
+        cursor: "pointer",
+        color: isVisible ? "#3e251e" : "inherit",
+        fontFamily: '"Themysion", sans-serif',
+        marginLeft: "1rem",
+        fontWeight: "100",
+        marginBottom: "-2rem",
+        marginTop: "1.5rem",
+      }}
+    >
+      {title}
+      {isVisible ? " - " : " + "}
+    </button>
+  );
+
   return (
     <div style={serviceStyle}>
       <div className="welcome page-title">
@@ -18,82 +53,108 @@ const Services = () => {
           to speak in more depth on pricing)
         </p>
       </div>
-      <section>
-        <h3>Bride</h3>
-        <h2 className="sub-title">Bridal Trial</h2>
-        <h2 className="sub-title">Registry MakeUp</h2>
-        <h2 className="sub-title">Wedding Day / Traditional Engagement</h2>
-        <h3>Bridesmaids / Additional Persons</h3>
-        <p className="tiny-text">
-          In my professional opinion, if you have a bridal party of more than 5,
-          it is best to book more than one artist.{" "}
-        </p>
-        <p className="tiny-text">
-          If you would like for me to make recommendations, please let me know
-          know.
-        </p>
-        <p className="maids-discount">
-          For a group of 3, the rate will be reduced by 10%. For a group of 5,
-          the rate will be reduced by 15%
-        </p>
+      <section onClick={toggleBrideVisibility}>
+        {renderSectionTitle("Bride", isBrideVisible)}
+        {isBrideVisible && (
+          <div className="packages">
+            <h2 className="more-sub-title">Bridal Trial</h2>
+            <h2 className="more-sub-title">Registry MakeUp</h2>
+            <h2 className="more-sub-title">
+              Wedding Day / Traditional Engagement
+            </h2>
+          </div>
+        )}
       </section>
-      <section>
-        <h3>Packages</h3>
-        <h2 className="sub-title">BRONZE</h2>
-        <p className="maids-discount">
-          This includes Bridal Trial, Registry MakeUp and Traditional or Wedding
-          Day MakeUp
-        </p>
-        <h2 className="sub-title">SILVER</h2>
-        <p className="maids-discount">
-          This includes Wedding Day or Traditional Engagement MakeUp (Plus 4Hrs
-          Touch Up)
-        </p>
-        <h2 className="sub-title">GOLD</h2>
-        <p className="maids-discount">
-          This includes Traditional Engagement or Wedding Day (Plus 8Hrs Touch
-          Up and change of look){" "}
-        </p>
-        <h2 className="sub-title">PLATINUM</h2>
-        <p className="maids-discount">
-          This includes Bridal Trial, Traditional Engagement, Wedding Day,
-          Mother of the Bride (Plus 4Hrs Touch Up){" "}
-        </p>
-        <h2 className="sub-title">PLATINUM PLUS+</h2>
-        <p className="maids-discount">
-          This includes Bridal Trial, Traditional Engagement, Wedding Day,
-          Mother of the Bride (Plus 8Hrs Touch Up and change of look){" "}
-        </p>
+      <section onClick={toggleBridesmaidsVisibility}>
+        {renderSectionTitle(
+          "Bridesmaids / Additional Persons",
+          isBridesmaidsVisible
+        )}
+        {isBridesmaidsVisible && (
+          <div className="packages">
+            <p className="maids-info">
+              In my professional opinion, if you have a bridal party of more
+              than 5, it is best to book more than one artist.{" "}
+            </p>
+            <p className="maids-infot">
+              If you would like for me to make recommendations, please let me
+              know know.
+            </p>
+            <p className="maids-info">
+              For a group of 3, the rate will be reduced by 10%. For a group of
+              5, the rate will be reduced by 15%
+            </p>
+          </div>
+        )}
       </section>
-      <section>
-        <h3>International Packages</h3>
-        <h2 className="sub-title">Bridal Bundle</h2>
-        <p className="maids-discount">
-          This includes the GOLD PACKAGE for the Bride + 2 additional persons
-          (Optional)
-        </p>
-        <h2 className="sub-title">Bridesmaid Bundle</h2>
-        <p className="maids-discount">
-          This includes up to 5 Bridesmaids/Additional Persons
-        </p>
-        <div className="fancy">
-          <img src={fancy} />
-          <p className="maids-discount">
-            Touch ups begin after initial application
-          </p>
-          <p className="maids-discount">
-            Additional time is charged at £35 per hour outside of the hours
-            included in the package
-          </p>
-          <p className="maids-discount">
-            Change of look includes; retouching base, eye look change and lip
-            colour change
-          </p>
-          <p className="maids-discount">
-            THIS APPLIES FOR INTERNATIONAL BOOKINGS
-          </p>
-          <img src={fancy} />
-        </div>
+      <section onClick={togglePackagesVisibility}>
+        {renderSectionTitle("Packages", isPackagesVisible)}{" "}
+        {isPackagesVisible && (
+          <div className="packages">
+            <h2 className="more-sub-title">BRONZE</h2>
+            <p className="maids-discount">
+              This includes Bridal Trial, Registry MakeUp and Traditional or
+              Wedding Day MakeUp
+            </p>
+            <h2 className="more-sub-title">SILVER</h2>
+            <p className="maids-discount">
+              This includes Wedding Day or Traditional Engagement MakeUp (Plus
+              4Hrs Touch Up)
+            </p>
+            <h2 className="more-sub-title">GOLD</h2>
+            <p className="maids-discount">
+              This includes Traditional Engagement or Wedding Day (Plus 8Hrs
+              Touch Up and change of look){" "}
+            </p>
+            <h2 className="more-sub-title">PLATINUM</h2>
+            <p className="maids-discount">
+              This includes Bridal Trial, Traditional Engagement, Wedding Day,
+              Mother of the Bride (Plus 4Hrs Touch Up){" "}
+            </p>
+            <h2 className="more-sub-title">PLATINUM PLUS+</h2>
+            <p className="maids-discount">
+              This includes Bridal Trial, Traditional Engagement, Wedding Day,
+              Mother of the Bride (Plus 8Hrs Touch Up and change of look){" "}
+            </p>
+          </div>
+        )}
+      </section>
+      <section onClick={toggleInternationalPackagesVisibility}>
+        {renderSectionTitle(
+          "International Packages",
+          isInternationalPackagesVisible
+        )}
+        {isInternationalPackagesVisible && (
+          <div className="packages">
+            <h2 className="more-sub-title">Bridal Bundle</h2>
+            <p className="maids-discount">
+              This includes the GOLD PACKAGE for the Bride + 2 additional
+              persons (Optional)
+            </p>
+            <h2 className="more-sub-title">Bridesmaid Bundle</h2>
+            <p className="maids-discount">
+              This includes up to 5 Bridesmaids/Additional Persons
+            </p>
+            <div className="fancy">
+              <img src={fancy} />
+              <p className="maids-discount">
+                Touch ups begin after initial application
+              </p>
+              <p className="maids-discount">
+                Additional time is charged at £35 per hour outside of the hours
+                included in the package
+              </p>
+              <p className="maids-discount">
+                Change of look includes; retouching base, eye look change and
+                lip colour change
+              </p>
+              <p className="maids-discount">
+                THIS APPLIES FOR INTERNATIONAL BOOKINGS
+              </p>
+              <img src={fancy} />
+            </div>
+          </div>
+        )}
       </section>
     </div>
   );
--- src/pages/TermsCon.js
+++ src/pages/TermsCon.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import background from "../images/background.png";
 
 const termStyle = {
@@ -11,6 +11,25 @@ const termStyle = {
 };
 
 const TermsPage = () => {
+  const [isPricesVisible, setPricesVisible] = useState(false);
+  const [isTravelVisible, setTravelVisible] = useState(false);
+  const [isLatenessVisible, setLatenessVisible] = useState(false);
+  const [isSicknessVisible, setSicknessVisible] = useState(false);
+  const [isCancellationVisible, setCancellationVisible] = useState(false);
+  const [isGroomingVisible, setGroomingVisible] = useState(false);
+  const [isPicturesVisible, setPicturesVisible] = useState(false);
+
+  const renderSubTitle = (title, isVisible, toggleVisibility) => (
+    <h2
+      className="sub-title"
+      style={{ cursor: "pointer", color: isVisible ? "#3e251e" : "inherit" }}
+      onClick={toggleVisibility}
+    >
+      {title}
+      {isVisible ? " - " : " + "}
+    </h2>
+  );
+
   return (
     <div style={termStyle}>
       <div className="terms-cond">
@@ -27,140 +46,174 @@ const TermsPage = () => {
       </div>
 
       <section>
-        <h2 className="sub-title">PRICES & PAYMENT</h2>
-        <ul>
-          <li>
-            All prices are for bookings made in 2023 and first quarter of 2024.
-          </li>
-          <li>
-            A non-refundable retainer of 50% is required and must be paid
-            through bank transfer for the booking to be confirmed. If you decide
-            you no longer want to go ahead with the booking, there is a grace
-            period of 3DAYS.
-          </li>
-          <li>
-            The remaining balance for non bridal / bridesmaid bookings must be
-            paid 24HRs before the booking and bridal / bridesmaid bookings will
-            be 1Week before the event.
-          </li>
-          <li>All payments are via bank transfer</li>
-        </ul>
-        <h2 className="sub-title">UK / INTERNATIONAL TRAVEL</h2>
-        <ul>
-          <li>
-            I charge a travel fee for all home bookings, this is listed as an
-            add on, on the booking page. Travel from my base that exceeds a 2
-            hour drive will need to be discussed seperately.
-          </li>
-          <li>All other bookings are take place in Leyton, East London.</li>
-          <li>
-            For international bookings, please note that I will require fully
-            paid direct return flights from Heathrow or London City Airport. 4*
-            minimum accommodation with breakfast and transport between the
-            airport, hotel and venue will need to be provided.
-          </li>
-          <li>
-            Checkout time from hotel must be a maximum of 2HRs from pick up time
-            on the departure day.
-          </li>
-          <li>
-            Accommodation close to the the event venue may be required the night
-            before the event if the journey time exceeds 2HRs one way or the
-            start time is 6AM or earlier (also if there are any other
-            foreseeable events/delays).
-          </li>
-          <li>
-            For any additional days, £100 is required for each day that I am in
-            the destination country but not including any services provided.
-          </li>
-          <li>
-            All COVID-19 tests needed for me and any other persons assisting me
-            during the international booking must be paid for by yourself (This
-            includes ad Day 5 test-to-release PCR test).
-          </li>
-        </ul>
-        <h2 className="sub-title">LATENESS</h2>
-        <ul>
-          <li>
-            If you are running late for any reason, please contact me as soon as
-            possible.
-          </li>
-          <li>
-            If you are 15 minutes late, please be advised there will a £10 late
-            fee which must be paid on arrival. If you are 20 minutes late, you
-            run the risk of your booking being cancelled and payment being
-            forfeited.
-          </li>
-        </ul>
-        <h2 className="sub-title">SICKNESS</h2>
-        <ul>
-          <li>
-            If you or those booking under your name have any COVID-19/Flu
-            symptoms or feel sick (e.g coughing, sneezing, vomiting etc. not
-            including hay fever/allergies) please refrain from booking until
-            you/they are fully recovered. If you notice the symptoms after
-            confirming the booking, please notify me ASAP so we can reschedule
-            within the next month.
-          </li>
-          <li>
-            If you come to the booking showing these symptoms without informing
-            me I will have to cancel the booking and your retainer won ’t be
-            refunded. You will need to book again once fully recovered.
-          </li>
-        </ul>
-        <h2 className="sub-title">CANCELLATIONS / RESCHUDLING</h2>
-        <ul>
-          <li>
-            If you decide to cancel/reschedule your event after the 3 day grace
-            period the retainer remains non-refundable. • Due to the uncertainty
-            regarding COVID-19, please note that you are booking at your own
-            risk and retainers remain non- refundable/transferable after 3 days
-            grace period. All rescheduled
-          </li>
-          <li>
-            All rescheduled bookings due to Government enforced lockdown are
-            subject to availability. In the event that I am not available, the
-            retainer remains non-refundable. For STANDARD BOOKINGS clients can
-            only reschedule within the year, and for BRIDAL BOOKINGS clients may
-            reschedule within the year or the following year.
-          </li>
-          <li>
-            Please notify me of any changes with the time slots, location or
-            number of clients for the booking at least 3 days before the
-            appointment, if not the retainer remains non-refundable if I am
-            unable to attend due to the changes.
-          </li>
-          <li>
-            If I am unable to perform my services for any personal reason, the
-            retainer will be fully refunded within 3 days.
-          </li>
-        </ul>
-        <h2 className="sub-title">GROOMING & ALLERGIES</h2>
-        <ul>
-          <li>
-            Please ensure that all unwanted facial hair has been removed and
-            your brows are groomed (to your preference) before the booking for a
-            clean finish.
-          </li>
-          <li>
-            If there are any brands, products or ingredients that you or anyone
-            booked under your name are allergic to, please notify me when
-            booking to avoid any reactions.
-          </li>
-          <li>
-            If you are coming to my studio, please let me know if you have
-            allergies or are scared of dogs.
-          </li>
-        </ul>
-        <h2 className="sub-title">PICTURES & VIDEOS</h2>
-        <ul>
-          <li>
-            Pictures and videos may be taken during and after the service is
-            complete. These images may be uploaded onto my social media pages
-            for content/promotional purposes. If you wish to not have any images
-            taken or just uploaded, please inform me before the service begins.
-          </li>
-        </ul>
+        {renderSubTitle("Prices & Payment", isPricesVisible, () =>
+          setPricesVisible(!isPricesVisible)
+        )}
+        {isPricesVisible && (
+          <ul>
+            <li>
+              All prices are for bookings made in 2023 and first quarter of
+              2024.
+            </li>
+            <li>
+              A non-refundable retainer of 50% is required and must be paid
+              through bank transfer for the booking to be confirmed. If you
+              decide you no longer want to go ahead with the booking, there is a
+              grace period of 3DAYS.
+            </li>
+            <li>
+              The remaining balance for non bridal / bridesmaid bookings must be
+              paid 24HRs before the booking and bridal / bridesmaid bookings
+              will be 1Week before the event.
+            </li>
+            <li>All payments are via bank transfer</li>
+          </ul>
+        )}
+        {renderSubTitle("UK / International Travel", isTravelVisible, () =>
+          setTravelVisible(!isTravelVisible)
+        )}
+        {isTravelVisible && (
+          <ul>
+            <li>
+              I charge a travel fee for all home bookings, this is listed as an
+              add on, on the booking page. Travel from my base that exceeds a 2
+              hour drive will need to be discussed seperately.
+            </li>
+            <li>All other bookings are take place in Leyton, East London.</li>
+            <li>
+              For international bookings, please note that I will require fully
+              paid direct return flights from Heathrow or London City Airport.
+              4* minimum accommodation with breakfast and transport between the
+              airport, hotel and venue will need to be provided.
+            </li>
+            <li>
+              Checkout time from hotel must be a maximum of 2HRs from pick up
+              time on the departure day.
+            </li>
+            <li>
+              Accommodation close to the the event venue may be required the
+              night before the event if the journey time exceeds 2HRs one way or
+              the start time is 6AM or earlier (also if there are any other
+              foreseeable events/delays).
+            </li>
+            <li>
+              For any additional days, £100 is required for each day that I am
+              in the destination country but not including any services
+              provided.
+            </li>
+            <li>
+              All COVID-19 tests needed for me and any other persons assisting
+              me during the international booking must be paid for by yourself
+              (This includes ad Day 5 test-to-release PCR test).
+            </li>
+          </ul>
+        )}
+        {renderSubTitle("Lateness", isLatenessVisible, () =>
+          setLatenessVisible(!isLatenessVisible)
+        )}
+        {isLatenessVisible && (
+          <ul>
+            <li>
+              If you are running late for any reason, please contact me as soon
+              as possible.
+            </li>
+            <li>
+              If you are 15 minutes late, please be advised there will a £10
+              late fee which must be paid on arrival. If you are 20 minutes
+              late, you run the risk of your booking being cancelled and payment
+              being forfeited.
+            </li>
+          </ul>
+        )}
+        {renderSubTitle("Sickness", isSicknessVisible, () =>
+          setSicknessVisible(!isSicknessVisible)
+        )}
+        {isSicknessVisible && (
+          <ul>
+            <li>
+              If you or those booking under your name have any COVID-19/Flu
+              symptoms or feel sick (e.g coughing, sneezing, vomiting etc. not
+              including hay fever/allergies) please refrain from booking until
+              you/they are fully recovered. If you notice the symptoms after
+              confirming the booking, please notify me ASAP so we can reschedule
+              within the next month.
+            </li>
+            <li>
+              If you come to the booking showing these symptoms without
+              informing me I will have to cancel the booking and your retainer
+              won ’t be refunded. You will need to book again once fully
+              recovered.
+            </li>
+          </ul>
+        )}
+        {renderSubTitle(
+          "Cancellations / Reschudiling",
+          isCancellationVisible,
+          () => setCancellationVisible(!isCancellationVisible)
+        )}
+        {isCancellationVisible && (
+          <ul>
+            <li>
+              If you decide to cancel/reschedule your event after the 3 day
+              grace period the retainer remains non-refundable. • Due to the
+              uncertainty regarding COVID-19, please note that you are booking
+              at your own risk and retainers remain non- refundable/transferable
+              after 3 days grace period. All rescheduled
+            </li>
+            <li>
+              All rescheduled bookings due to Government enforced lockdown are
+              subject to availability. In the event that I am not available, the
+              retainer remains non-refundable. For STANDARD BOOKINGS clients can
+              only reschedule within the year, and for BRIDAL BOOKINGS clients
+              may reschedule within the year or the following year.
+            </li>
+            <li>
+              Please notify me of any changes with the time slots, location or
+              number of clients for the booking at least 3 days before the
+              appointment, if not the retainer remains non-refundable if I am
+              unable to attend due to the changes.
+            </li>
+            <li>
+              If I am unable to perform my services for any personal reason, the
+              retainer will be fully refunded within 3 days.
+            </li>
+          </ul>
+        )}
+        {renderSubTitle("Grooming & Allergies", isGroomingVisible, () =>
+          setGroomingVisible(!isGroomingVisible)
+        )}
+        {isGroomingVisible && (
+          <ul>
+            <li>
+              Please ensure that all unwanted facial hair has been removed and
+              your brows are groomed (to your preference) before the booking for
+              a clean finish.
+            </li>
+            <li>
+              If there are any brands, products or ingredients that you or
+              anyone booked under your name are allergic to, please notify me
+              when booking to avoid any reactions.
+            </li>
+            <li>
+              If you are coming to my studio, please let me know if you have
+              allergies or are scared of dogs.
+            </li>
+          </ul>
+        )}
+        {renderSubTitle("Pictures & Videos", isPicturesVisible, () =>
+          setPicturesVisible(!isPicturesVisible)
+        )}
+        {isPicturesVisible && (
+          <ul>
+            <li>
+              Pictures and videos may be taken during and after the service is
+              complete. These images may be uploaded onto my social media pages
+              for content/promotional purposes. If you wish to not have any
+              images taken or just uploaded, please inform me before the service
+              begins.
+            </li>
+          </ul>
+        )}
       </section>
 
       <div className="bottom-text">
